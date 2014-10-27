ead1a73711dccccf9b86f06dd371f4bb220f34ff
Ryan <ryabouza@gmail.com>
2014-10-27 12:32:41 -0400

    Simple script to power cycle nodes

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setup(name='haas',
       version='1.0',
       url='https://github.com/CCI-MOC/haas',
       packages=find_packages(),
-      scripts=['scripts/haas', 'scripts/create_bridges'],
+      scripts=['scripts/haas', 'scripts/create_bridges', 'scripts/power_cycle_nodes'],
       install_requires=requirements,
       )
