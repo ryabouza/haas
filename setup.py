71966034294b901728e29c88e3650de643b0cc9b
Ryan <ryabouza@gmail.com>
2014-10-28 15:21:33 -0400

    Fill out metadata in setup.py
    This is in response to issue #321

--- setup.py
+++ setup.py
@@ -14,13 +14,33 @@
 
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
+import os
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
 requirements = [str(r.req) for r in parse_requirements('requirements.txt')]
 
 setup(name='haas',
       version='1.0',
+      description='HaaS is a low-level tool for reserving physical machines'
+                  ' and connecting them via isolated networks.',
+      long_description=read('README.rst'),
+      author='Massachusetts Open Cloud Contributors',
+      author_email='moc-technical@googlegroups.com',
+      license='Apache 2.0',
       url='https://github.com/CCI-MOC/haas',
       packages=find_packages(),
       scripts=['scripts/haas', 'scripts/create_bridges'],
       install_requires=requirements,
-      )
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console'
+          'Topic :: System :: Systems Administration',
+          'Intended Audience :: System Administrators',
+          'Natural Language :: English',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2.6',
+          'License :: OSI Approved :: Apache Software License',
+      ],
+)
